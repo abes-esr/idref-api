e046dad139795c8b53e4bbd377b8c20222624181
@xhermesx <chevallier.corp@gmail.com>
2020-09-24 15:37:23 +0200

    commit swagger

--- src/main/java/fr/idref/idrefapi/configuration/SwaggerConfig.java
+++ src/main/java/fr/idref/idrefapi/configuration/SwaggerConfig.java
@@ -11,6 +11,11 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+
+    /**
+     * Swagger config
+     * @return
+     */
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
